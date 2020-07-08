f135a26d43a7e9215ab467ce49b2ca67abc42f74
mauro.gomez <mauro.gomez@zabud.com.co>
2020-07-08 10:57:43 -0500

    first blood

---
+++ src/main/java/com/zabud/jolt/JoltPocApplication.java
@@ -0,0 +1,13 @@
+package com.zabud.jolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoltPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoltPocApplication.class, args);
+	}
+
+}
