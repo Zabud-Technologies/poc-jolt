b747a060aca8dd0a55861dc2124cf02c603b0e5d
mauro.gomez <mauro.gomez@zabud.com.co>
2020-07-22 09:19:59 -0500

    espec,  remover codigo ejemplo

--- src/main/java/com/zabud/jolt/JoltPocApplication.java
+++ src/main/java/com/zabud/jolt/JoltPocApplication.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/zabud/jolt/domain/ProfessionalBoardHeaderDTO.java
+++
@@ -1,9 +0,0 @@
-package com.zabud.jolt.domain;
-
-import lombok.Data;
-
-@Data
-public class ProfessionalBoardHeaderDTO {
-  
-  ProfessionalBoardDTO professionalBoardDTO;
-}
